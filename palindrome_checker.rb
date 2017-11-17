fbf27c90e7c8c93c2e1f311b789a685d507711dd
Bryan Leanoe <bryan.leano@gmail.com>
2017-11-17 10:20:58 -0700

    upload

---
+++ palindrome_checker.rb
@@ -0,0 +1,11 @@
+
+
+@answer = []
+
+puts "Welcome to Palindrome Checker!"
+puts "Please enter a word to see if it's a Palindrome"
+answer = gets.strip.to_s
+values = answer.split("")
+@answer << values
+puts "hello"
+    
